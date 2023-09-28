5baa09d911244e312433ce2c5561a1db4de8a647
DecentM <decentm@decentm.com>
2023-09-29 00:53:54 +0300

    build: fix issues from actions run

--- packages/core/src/notation/writer.ts
+++ packages/core/src/notation/writer.ts
@@ -1,113 +1,108 @@
-import { Node, RootNode } from "./parser";
-import { fileToLetter } from "../lib/notation";
+import { Node, RootNode } from './parser'
+import { fileToLetter } from '../lib/notation'
 
 export const writeNode = (node: Node): string => {
   switch (node.kind) {
-    case "draw-offer":
-      return "(=)";
+    case 'draw-offer':
+      return '(=)'
 
-    case "game-over":
+    case 'game-over': {
       switch (node.outcome) {
-        case "black":
-          return "0-1";
+        case 'black':
+          return '0-1'
 
-        case "white":
-          return "1-0";
+        case 'white':
+          return '1-0'
 
-        case "draw":
-          return "1/2-1/2";
+        case 'draw':
+          return '1/2-1/2'
 
-        case "default":
-          return "-/-";
+        case 'forfeit':
+          return '0-0'
 
-        case "default-black":
-          return "-/+";
+        case 'forfeit-black':
+          return '0-1/2'
 
-        case "default-white":
-          return "+/-";
-
-        case "forfeit":
-          return "0-0";
-
-        case "forfeit-black":
-          return "0-1/2";
-
-        case "forfeit-white":
-          return "1/2-0";
+        case 'forfeit-white':
+          return '1/2-0'
       }
 
-    case "move":
+      break
+    }
+
+    case 'move': {
       const writeMove = (action: string, suffix?: string) => {
-        let result = "";
+        let result = ''
 
         if (node.piece) {
-          result += node.piece;
+          result += node.piece
         }
 
         if (node.from.file) {
-          result += `${fileToLetter(node.from.file)}`;
+          result += `${fileToLetter(node.from.file)}`
         }
 
         if (node.from.rank) {
-          result += `${node.from.rank}`;
+          result += `${node.from.rank}`
         }
 
-        result += action;
+        result += action
 
         if (node.to.file) {
-          result += `${fileToLetter(node.to.file)}`;
+          result += `${fileToLetter(node.to.file)}`
         }
 
         if (node.to.rank) {
-          result += `${node.to.rank}`;
+          result += `${node.to.rank}`
         }
 
-        if (suffix) result += suffix;
+        if (suffix) result += suffix
 
-        return result;
-      };
+        return result
+      }
 
       switch (node.type) {
-        case "allegiance":
-          return writeMove(">");
+        case 'allegiance':
+          return writeMove('>')
 
-        case "capture":
-          return writeMove("x");
+        case 'capture':
+          return writeMove('x')
 
-        case "castle":
-          return node.side === "king" ? "O-O" : "O-O-O";
+        case 'castle':
+          return node.side === 'king' ? 'O-O' : 'O-O-O'
 
-        case "en-passant":
-          return writeMove("x", " e.p.");
+        case 'en-passant':
+          return writeMove('x', ' e.p.')
 
-        case "promotion":
-          return writeMove("=");
+        case 'promotion':
+          return writeMove('=')
 
         default:
-          return writeMove("");
+          return writeMove('')
       }
+    }
   }
-};
+}
 
 export const write = (root: RootNode): string => {
-  const steps: string[] = [];
+  const steps: string[] = []
 
   root.children.forEach((node, moveIndex) => {
-    const whitesMove = moveIndex % 2 === 0;
+    const whitesMove = moveIndex % 2 === 0
 
     const write = (move: string) => {
       if (whitesMove) {
-        steps.push(move);
+        steps.push(move)
       } else {
-        steps[steps.length - 1] += ` ${move}`;
+        steps[steps.length - 1] += ` ${move}`
       }
-    };
+    }
 
-    write(writeNode(node));
-  });
+    write(writeNode(node))
+  })
 
   return steps
     .filter(Boolean)
     .map((step, index) => `${index + 1}. ${step}`)
-    .join("\n");
-};
+    .join('\n')
+}
