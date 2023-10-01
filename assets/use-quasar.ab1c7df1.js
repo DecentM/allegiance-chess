8cea685489ddca6f6221d4a4f87562b9fc4f0ad2
DecentM <DecentM@users.noreply.github.com>
2023-10-01 23:54:37 +0000

    deploy: d99b1d8c0375be7a2bcf620daab03de611b3ff7d

--- assets/use-quasar.ab1c7df1.js
+++
@@ -1,1 +0,0 @@
-import{i as a,ak as r}from"./index.24610c7a.js";function u(){return a(r)}export{u};
---
+++ assets/use-quasar.c372a953.js
@@ -0,0 +1,1 @@
+import{i as a,ak as r}from"./index.9b88f190.js";function u(){return a(r)}export{u};
