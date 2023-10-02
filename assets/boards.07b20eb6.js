e24f994505c7a132154668a7f8cc368ff2b1bb5b
DecentM <DecentM@users.noreply.github.com>
2023-10-02 05:37:42 +0000

    deploy: 6cc85d59b77c2ba48a6cc4b19b12972d94d1e03f

--- assets/boards.07b20eb6.js
+++ assets/boards.07b20eb6.js
@@ -1,1 +1,2 @@
 const p={VanillaDefault:"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0",GreyDefault:"r>n>b>q>kb>n>r>/p>p>p>p>p>p>p>p>/8/8/8/8/P>P>P>P>P>P>P>P>/R>N>B>Q>KB>N>R> w KQkq - 0 0"};export{p as F};
+//# sourceMappingURL=boards.07b20eb6.js.map
--- assets/use-quasar.7dad5e48.js
+++ assets/use-quasar.7dad5e48.js
@@ -1,1 +1,2 @@
 import{k as a,ak as r}from"./index.a9cd5dcf.js";function u(){return a(r)}export{u};
+//# sourceMappingURL=use-quasar.7dad5e48.js.map
