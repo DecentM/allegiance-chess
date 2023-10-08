37fd17e581b3f32af3306e7b0dc20f3dfacd5c4e
DecentM <DecentM@users.noreply.github.com>
2023-10-08 08:55:58 +0000

    deploy: 0c16399464690e8a22262cbd0cd74cfdaaf5a8f4

--- assets/notify.5c7bb6ab.js
+++
@@ -1,2 +0,0 @@
-import{u as i}from"./use-quasar.834f62df.js";const r=()=>{const t=i();return{notify:o=>{t.notify({timeout:4e3,position:"bottom-right",...o})}}};export{r as u};
-//# sourceMappingURL=notify.5c7bb6ab.js.map
