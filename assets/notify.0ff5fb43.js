61e13b828a312f675d02b2976bae1effab39d6a7
DecentM <DecentM@users.noreply.github.com>
2023-10-09 04:03:32 +0000

    deploy: d7d1e75d2d38a912685bc69771d0c312c0650e91

---
+++ assets/notify.0ff5fb43.js
@@ -0,0 +1,2 @@
+import{u as i}from"./use-quasar.07a0740a.js";const r=()=>{const t=i();return{notify:o=>{t.notify({timeout:4e3,position:"bottom-right",...o})}}};export{r as u};
+//# sourceMappingURL=notify.0ff5fb43.js.map
---
+++ assets/use-quasar.07a0740a.js
@@ -0,0 +1,2 @@
+import{i as a,U as r}from"./index.d98542eb.js";function u(){return a(r)}export{u};
+//# sourceMappingURL=use-quasar.07a0740a.js.map
